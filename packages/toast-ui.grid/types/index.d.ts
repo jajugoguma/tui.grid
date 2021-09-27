49cd21f58245e9deb8237bfd1ad08a1aa35fc465
jajugoguma <jajugoguma@gmail.com>
2021-09-27 17:06:45 +0900

    chore: 17

--- packages/toast-ui.grid/types/index.d.ts
+++ packages/toast-ui.grid/types/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for TOAST UI Grid v1.0.16
+// Type definitions for TOAST UI Grid v1.0.17
 // TypeScript Version: 3.9.5
 
 import { CellValue, RowKey, Row, SortState, RowSpan, InvalidRow } from './store/data';
