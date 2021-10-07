95aeeeeb16b9d538e5bcde9dfb55a7d52268e893
jajugoguma <daeyeon.kim@nhn.com>
2021-10-07 20:01:04 +0900

    cchore: test

--- packages/toast-ui.grid/types/index.d.ts
+++ packages/toast-ui.grid/types/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for TOAST UI Grid v1.0.21
+// Type definitions for TOAST UI Grid v1.0.22
 // TypeScript Version: 3.9.5
 
 import { CellValue, RowKey, Row, SortState, RowSpan, InvalidRow } from './store/data';
